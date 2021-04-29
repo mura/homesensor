8948657f8afe4ca4c007358b9b1746dbf72a107f
Yohei Murayama <muracchi@gmail.com>
2021-04-29 23:55:35 +0900

    Firebaseのプロジェクトに変更

---
+++ firebase/functions/src/index.ts
@@ -0,0 +1,36 @@
+"use strict";
+
+import * as functions from "firebase-functions";
+import {BigQuery} from "@google-cloud/bigquery";
+
+const bigquery = new BigQuery();
+
+export const insertBigQuery = functions.pubsub.topic("sensors")
+    .onPublish((message, context) => {
+      const json = Buffer.from(message.data, "base64").toString();
+      // console.log(json);
+      if (json.match(/-connected$/)) {
+        return;
+      }
+      const deviceId = message.attributes.deviceId;
+      const payload = JSON.parse(json);
+      // console.log(`${deviceId} Payload: ${JSON.stringfy(payload)}`);
+      // console.log(context);
+
+      if (payload) {
+        const row = {
+          device_id: deviceId,
+          co2: payload.c,
+          temperature: payload.t,
+          humidity: payload.h,
+          pressure: payload.p,
+          time: context.timestamp,
+        };
+        return bigquery
+            .dataset("sensors")
+            .table("atmosphere")
+            .insert(row);
+        // console.log('Inserted: ' + row);
+      }
+      return 0;
+    });
